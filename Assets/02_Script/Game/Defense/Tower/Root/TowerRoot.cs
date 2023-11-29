8a93dd0f66b5df4e3b11e50512d308b73efa0c95
dkdkdsa <bos94330@gmail.com>
2023-11-29 23:37:19 +0900

    TowerRoot

--- Assets/02_Script/Game/Defense/Tower/Root/TowerRoot.cs
+++ Assets/02_Script/Game/Defense/Tower/Root/TowerRoot.cs
@@ -3,8 +3,32 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class TowerRoot : NetworkBehaviour
+public class LevelData
 {
+
+    public float attackPower;
+    public float attackRange;
+    public float attackCoolDown;
+    public int levelUpCost;
+
+}
+
+public abstract class TowerRoot : NetworkBehaviour
+{
+
+    [SerializeField] protected List<LevelData> levelData = new();
+
+    protected int curLv;
+    protected bool isAttackCoolDown;
+
+    protected virtual void Update()
+    {
+
+        if (!IsOwner) return;
+
+    }
+
+    protected abstract void DoAttack();
 
     [ClientRpc]
     public void SetPosClientRPC(Vector2 pos, ulong clientId)
@@ -13,6 +37,13 @@ public class TowerRoot : NetworkBehaviour
         var cPos = clientId == NetworkManager.Singleton.LocalClientId ? pos : -pos;
 
         transform.position = cPos;
+
+    }
+
+    private IEnumerator AttackDelayCo()
+    {
+
+        
 
     }
 
