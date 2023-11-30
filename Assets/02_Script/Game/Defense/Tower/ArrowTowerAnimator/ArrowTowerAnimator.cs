5a4e85e327346fd577c0951125998cc48faa5240
dkdkdsa <bos94330@gmail.com>
2023-12-01 00:37:10 +0900

    ArrowTowerAnimator

--- Assets/02_Script/Game/Defense/Tower/ArrowTowerAnimator/ArrowTowerAnimator.cs
+++ Assets/02_Script/Game/Defense/Tower/ArrowTowerAnimator/ArrowTowerAnimator.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class ArrowTowerAnimator : MonoBehaviour
 {
 
-    private readonly int HASH_ATTACK;
+    private readonly int HASH_ATTACK = Animator.StringToHash("Attack");
 
     private Animator animator;
+
+    public event Action OnAttackAnimeEnd;
 
     private void Awake()
     {
         
         animator = GetComponent<Animator>();
+
+    }
+
+    public void SetAttackAnime()
+    {
+        
+        animator.SetTrigger(HASH_ATTACK);
+        
+    }
+
+    public void AttackAnimeEndExecute()
+    {
+
+        OnAttackAnimeEnd?.Invoke();
 
     }
 
