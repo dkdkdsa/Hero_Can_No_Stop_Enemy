5fbc965b40d0c1468dde0517e0a20513e93ce349
dkdkdsa <bos94330@gmail.com>
2023-12-04 12:16:23 +0900

    DeckController

--- Assets/02_Script/Game/Player/PlayerDeckSettingController.cs
+++ Assets/02_Script/Game/Player/PlayerDeckSettingController.cs
@@ -6,17 +6,27 @@ public class PlayerDeckSettingController : MonoBehaviour
 {
 
     [SerializeField] private TowerListSO towerData;
-    [SerializeField] private SpriteRenderer towerSpawnArea;
+    [SerializeField] private SpriteRenderer towerSpawnAreaSprite;
     [SerializeField] private AreaObject towerCreateArea;
 
+    private List<TowerRoot> towers;
+    private Rect rect => new Rect(transform.position.x - 1 / 2, transform.position.y - 1 / 2, 1, 1);
     private bool isTowerCreating;
+    private bool createAble;
     private string towerKey;
-    private List<TowerObj> towers;
 
     private void Update()
     {
 
         if (!isTowerCreating) return;
+
+
+        if (Input.GetMouseButtonUp(0))
+        {
+
+            CreateTower();
+
+        }
 
     }
 
@@ -24,6 +34,31 @@ public class PlayerDeckSettingController : MonoBehaviour
     {
 
 
+
+    }
+
+    private void ChackCreateAble()
+    {
+
+
+        bool vel1 = towerCreateArea.ChackOverlaps(rect);
+
+        bool vel2 = true;
+
+        foreach(var area in towers)
+        {
+
+            if (area.towerArea.ChackOverlaps(rect))
+            {
+
+                vel2 =false; 
+                break;
+
+            }
+
+        }
+
+        createAble = vel1 && vel2;
 
     }
 
