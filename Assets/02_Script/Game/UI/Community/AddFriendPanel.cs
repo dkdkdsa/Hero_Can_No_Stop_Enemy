923e71f23fc2a661f1d815ec90da1a3b06980c99
dkdkdsa <bos94330@gmail.com>
2023-12-07 23:51:49 +0900

    AddFriendPanel

--- Assets/02_Script/Game/UI/Community/AddFriendPanel.cs
+++ Assets/02_Script/Game/UI/Community/AddFriendPanel.cs
@@ -1,10 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
+public delegate void ButtonClick();
 
 public class AddFriendPanel : MonoBehaviour
 {
-    
 
+    [SerializeField] private GameObject button;
+    [SerializeField] private TMP_Text panelText;
+
+    public event ButtonClick OnButtonClick;
+
+    public void SetPanel(string panelText, bool buttonUse)
+    {
+
+        this.panelText.text = panelText;
+
+        button.gameObject.SetActive(buttonUse);
+
+    }
+
+    public void ClickBtn()
+    {
+
+        OnButtonClick?.Invoke();
+
+    }
 
 }
