ed1ccc1042f1c4bb8d3e83bee13d0e1cb1cff5b5
dkdkdsa <bos94330@gmail.com>
2023-12-08 01:08:56 +0900

    CommunityUIController

--- Assets/02_Script/Game/UI/Community/CommunityUIController.cs
+++ Assets/02_Script/Game/UI/Community/CommunityUIController.cs
@@ -9,15 +9,26 @@ public class CommunityUIController : MonoBehaviour
     [SerializeField] private TMP_InputField userNameField;
     [SerializeField] private AddFriendPanel friendPreafab;
 
+
     public async void Serch()
     {
 
         var users = await FirebaseManager.Instance.GetAllUser();
 
-        foreach (var user in users)
+        var findUser = users.FindAll(x => x.userData.userName == userNameField.text);
+
+        if(findUser.Count != 0)
         {
 
+            foreach (var user in findUser)
+            {
 
+                var slot = Instantiate(friendPreafab);
+
+                slot.SetPanel(user.userData.userName, true);
+                slot.SetUserDataAndKey(user.key, user.userData);
+
+            }
 
         }
 
